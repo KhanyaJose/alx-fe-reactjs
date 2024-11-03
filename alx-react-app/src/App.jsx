bba5328179c3b115586da6ce711d7b4ab5d86346
Khanya <iamkhanyajose@gmail.com>
2024-11-03 18:41:57 +0200

    Added Header, MainContent &  Footer.jsx and also updated App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,11 +3,17 @@ import HelloWorld from './HelloWorld'; // Import HelloWorld component
 import UserProfileCard from './UserProfileCard'; // Import UserProfileCard component
 import Counter from './Counter'; // Import Counter component
 import WelcomeMessage from './components/WelcomeMessage';
+import Header from './Header'; // Import Header component
+import MainContent from './MainContent'; // Import MainContent component
+import Footer from './Footer'; // Import Footer component
 
 function App() {
     return (
         <div>
-            <WelcomeMessage/> {/* Include the WelcomeMessage component */}
+            <Header /> {/* Include Header */}
+            <MainContent /> {/* Include Main Content */}
+            <Footer /> {/* Include Footer */}
+            <WelcomeMessage /> {/* This is the WelcomeMessage component */}
             <HelloWorld />
             <UserProfileCard 
                 name="John Doe"
