aa4a357e6ddbdc11513902ee66c36138752e01fe
Khanya <iamkhanyajose@gmail.com>
2024-11-08 18:04:01 +0200

    Added changes for alx-react-app-new directory

---
+++ alx-react-app-new/src/App.jsx
@@ -0,0 +1,36 @@
+// src/App.jsx
+import Header from './components/Header'; // Import Header component
+import MainContent from './components/MainContent'; // Import MainContent component
+import Footer from './components/Footer'; // Import Footer component
+import HelloWorld from './HelloWorld'; // Import HelloWorld component
+import UserProfileCard from './UserProfileCard'; // Import UserProfileCard component
+import Counter from './Counter'; // Import Counter component
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile'; // Import UserProfile component
+
+function App() {
+    return (
+        <div>
+            <Header /> {/* Include Header */}
+            <MainContent /> {/* Include Main Content */}
+            <Footer /> {/* Include Footer */}
+
+            <WelcomeMessage /> {/* This is the WelcomeMessage component */}
+            <HelloWorld />
+            <UserProfileCard 
+                name="John Doe"
+                email="john.doe@example.com"
+                profilePicture="https://via.placeholder.com/100"
+            />
+            <Counter />
+            {/* Using UserProfile component with props */}
+            <UserProfile 
+                name="Alice" 
+                age="25" 
+                bio="Loves hiking and photography" 
+            />
+        </div>
+    );
+}
+
+export default App; // Export the App component
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,11 @@
+// src/MainContent.jsx
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', fontSize: '18px', color: 'darklategray' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
+
