f0495b26bb535787f22cf2a1a7d646494c8cb87b
Khanya <iamkhanyajose@gmail.com>
2024-11-16 19:28:49 +0200

    Madep some changes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
-import RecipeList from './components/RecipeList';
-import AddRecipeForm from './components/AddRecipeForm';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'; // Importing Router and routing components
+import RecipeList from './components/RecipeList'; // Recipe list component
+import RecipeDetails from './components/RecipeDetails'; // Recipe details component
+import AddRecipeForm from './components/AddRecipeForm'; // Add recipe form component
+import EditRecipeForm from './components/EditRecipeForm'; // Edit recipe form component (if you have this)
 
 const App = () => {
   return (
-    <div>
-      <h1>Recipe Sharing Application</h1>
-      <AddRecipeForm />
-      <RecipeList />
-    </div>
+    <Router>
+      <Routes>
+        {/* Define your routes */}
+        <Route path="/" element={<RecipeList />} /> {/* Home route to show all recipes */}
+        <Route path="/recipe/:id" element={<RecipeDetails />} /> {/* Route to show individual recipe details */}
+        <Route path="/add" element={<AddRecipeForm />} /> {/* Route for adding a new recipe */}
+        <Route path="/edit/:id" element={<EditRecipeForm />} /> {/* Route for editing a recipe */}
+      </Routes>
+    </Router>
   );
 };
 
