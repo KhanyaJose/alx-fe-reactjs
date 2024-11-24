fd7b10b3988d31aa17b9103d93dcea044460f944
Khanya <iamkhanyajose@gmail.com>
2024-11-24 23:46:00 +0200

    made changes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,20 +1,14 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Profile from './components/Profile';
-import ProfileDetails from './components/ProfileDetails';
-import ProfileSettings from './components/ProfileSettings';
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import ProtectedRoute from './components/ProtectedRoute';
+import BlogPost from './components/BlogPost'; // Import the BlogPost component
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/profile" element={<ProtectedRoute />}>
-          <Route index element={<Profile />} />
-          <Route path="details" element={<ProfileDetails />} />
-          <Route path="settings" element={<ProfileSettings />} />
-        </Route>
+        <Route path="/blog/:id" element={<BlogPost />} /> {/* Dynamic route for blog posts */}
       </Routes>
     </Router>
   );
--- react-router-advanced/src/components/BlogPost.jsx
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -1,8 +1,16 @@
+// src/components/BlogPost.jsx
 import { useParams } from 'react-router-dom';
 
 function BlogPost() {
-  const { postId } = useParams();
-  return <div><h2>Blog Post {postId}</h2></div>;
+  const { id } = useParams(); // Extracting the dynamic id from the URL
+
+  return (
+    <div>
+      <h1>Blog Post {id}</h1>
+      <p>This is the blog post with ID: {id}</p>
+      {/* You can fetch the post data from an API based on the ID here */}
+    </div>
+  );
 }
 
 export default BlogPost;
