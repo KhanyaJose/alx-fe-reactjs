1fbae0d9e4c159e0bae98b9812aded0aed6dd96f
Khanya <iamkhanyajose@gmail.com>
2024-11-24 23:44:09 +0200

    Made changes

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,9 +1,8 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/Home';
 import Profile from './components/Profile';
 import ProfileDetails from './components/ProfileDetails';
 import ProfileSettings from './components/ProfileSettings';
-import BlogPost from './components/BlogPost';
+import Home from './components/Home';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
@@ -12,10 +11,10 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<ProtectedRoute />}>
+          <Route index element={<Profile />} />
           <Route path="details" element={<ProfileDetails />} />
           <Route path="settings" element={<ProfileSettings />} />
         </Route>
-        <Route path="/blog/:postId" element={<BlogPost />} />
       </Routes>
     </Router>
   );
