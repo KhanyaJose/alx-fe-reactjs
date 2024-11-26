2c72fda2a9aee9322967b0d5fed3d5fb217aa82d
Khanya <iamkhanyajose@gmail.com>
2024-11-26 06:51:22 +0200

    Made some chanhes

--- react-todo/src/components/ToDoList.jsx
+++ react-todo/src/components/ToDoList.jsx
@@ -42,5 +42,28 @@ const TodoList = () => {
     </div>
   );
 };
+function AddTodoForm({ addTodo }) {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim()) {
+      addTodo(input);
+      setInput('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a new todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit">Add</button>
+    </form>
+  );
+}
 
 export default TodoList;
