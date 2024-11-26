878b51408a31ae16357fee40c8be9ef9d0aaf808
Khanya <iamkhanyajose@gmail.com>
2024-11-26 06:43:03 +0200

    Made some changes

--- react-todo/src/__tests__/ToDoList.test.js
+++ react-todo/src/__tests__/ToDoList.test.js
@@ -1,5 +1,5 @@
 import { render, screen, fireEvent } from '@testing-library/react';
-import TodoList from '../components/TodoList';
+import TodoList from '../__tests__/TodoList';
 
 test('renders initial todo list', () => {
   render(<TodoList />);
