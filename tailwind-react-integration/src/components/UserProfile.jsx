0c51bf5a98fbd38ebcbd124ba287800f1be78b32
Khanya <iamkhanyajose@gmail.com>
2024-11-30 18:20:56 +0200

    Completed Task 3

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,12 +1,12 @@
 function UserProfile() {
     return (
-      <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-sm md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg">
+      <div className="user-profile bg-gray-100 p-4 sm:p-4 md:p-8 max-w-sm md:max-w-sm max-w-xs mx-auto my-20 rounded-lg shadow-lg hover:shadow-xl transition-shadow duration-300">
         <img
           src="https://via.placeholder.com/150"
           alt="User"
-          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto"
+          className="rounded-full sm:w-24 sm:h-24 md:w-36 md:h-36 mx-auto hover:scale-110 transition-transform duration-300 ease-in-out"
         />
-        <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4">John Doe</h1>
+        <h1 className="text-lg sm:text-xl md:text-xl text-blue-800 my-4 hover:text-blue-500">John Doe</h1>
         <p className="text-sm sm:text-base text-gray-600">
           Developer at Example Co. Loves to write code and explore new technologies.
         </p>
