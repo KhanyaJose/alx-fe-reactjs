6bdd5e64f4b0ec09ec33e8550f3f9a8db128b5ad
Khanya <iamkhanyajose@gmail.com>
2025-08-03 16:38:19 +0200

    Completed Task 0 by setting up GitHub User Search React App with Axios and its base structre

---
+++ github-user-search/src/App.jsx
@@ -0,0 +1,13 @@
+// src/App.jsx
+import React from 'react';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 text-center py-10">
+      <h1 className="text-3xl font-bold mb-4 text-blue-600">GitHub User Search</h1>
+      <p className="text-gray-700">Start typing to search for GitHub users...</p>
+    </div>
+  );
+}
+
+export default App;
